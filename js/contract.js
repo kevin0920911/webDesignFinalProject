21b883b097cbf936a15d70b339b0841cb45cf211
HSU SHENG KAI <83535048+kevin0920911@users.noreply.github.com>
2023-05-30 15:58:03 +0800

    功能修正
    功能修正

--- js/contract.js
+++ js/contract.js
@@ -6,7 +6,7 @@ function submit(){
     formdata.append("time", new Date());
     formdata.append("name", form.elements.name.value);
     formdata.append("email", form.elements.email.value);
-    formdata.append("phone", form.elements.phone.value);
+    formdata.append("phone", form.elements.phone.value.toString());
     formdata.append("message", form.elements.message.value);
 
     const config = { method: "POST", body: formdata, redirect: "follow" };
